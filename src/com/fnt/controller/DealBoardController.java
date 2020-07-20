44e7fa383cc0cc04f534e13e09f6a2c85a83b324
Kang YoeRim <yr9708@hanmail.net>
2020-07-20 16:27:13 +0900

    여림 : 다시 commit

--- src/com/fnt/controller/DealBoardController.java
+++ src/com/fnt/controller/DealBoardController.java
@@ -20,5 +20,5 @@ public class DealBoardController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
+ 
 }
