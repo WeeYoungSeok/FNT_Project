f94ddff9115a8dfbfb2fae630e090eef6e4500b3
WeeYoungSeok <64151499+WeeYoungSeok@users.noreply.github.com>
2020-08-10 15:09:25 +0900

    회원가입 팝업크기 조정

--- WebContent/js/fntsignupform.js
+++ WebContent/js/fntsignupform.js
@@ -4,7 +4,7 @@
 			alert("ID를 입력해주세요!");
 			return false;
 		} else {
-			window.open(url + '&id=' + $("#id").val(), name, "width=300, height=180");
+			window.open(url + '&id=' + $("#id").val(), name, "width=362, height=197");
 		}
 	}
 	
@@ -33,7 +33,7 @@
 			alert("Nickname을 입력해주세요!");
 			return false;
 		} else {
-			window.open(url + '&nick=' + $("#nick").val(), name, "width=330, height=170");
+			window.open(url + '&nick=' + $("#nick").val(), name, "width=330, height=180");
 		}
 	}
  	
@@ -74,7 +74,7 @@
  			alert("이메일을 입력해주세요.");
  			return false;
  		} else {
- 			window.open(url + getParameterEmail(), name, "width=300, height=170");
+ 			window.open(url + getParameterEmail(), name, "width=400, height=170");
  		}
  	}
 
